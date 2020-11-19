d8ef77fda8a17629d6cb62c451002a75576555d0
anxindong <anxdbj@163.com>
2020-11-19 14:03:19 +0800

    hystrix-dashboard测试通过

--- ribbon-consumer/src/main/java/com/example/ribbonconsumer/RibbonConsumerApplication.java
+++ ribbon-consumer/src/main/java/com/example/ribbonconsumer/RibbonConsumerApplication.java
@@ -2,11 +2,14 @@ package com.example.ribbonconsumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -17,6 +20,15 @@ public class RibbonConsumerApplication {
     @LoadBalanced
     RestTemplate restTemplate(){
         return new RestTemplate();
+    }
+    @Bean
+    public ServletRegistrationBean getServlet(){
+    	HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+    	ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+    	registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/actuator/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+    	return registrationBean;
     }
     public static void main(String[] args) {
         SpringApplication.run(RibbonConsumerApplication.class, args);
