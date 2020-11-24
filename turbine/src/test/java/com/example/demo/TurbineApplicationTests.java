ba01c2762e83f511e4cf35dbc7695a226c31e66c
anxindong <anxdbj@163.com>
2020-11-24 19:10:06 +0800

    turbine

--- turbine/src/test/java/com/example/demo/TurbineApplicationTests.java
+++ turbine/src/test/java/com/example/demo/TurbineApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class HystrixDashboardApplicationTests {
+class TurbineApplicationTests {
 
 	@Test
 	void contextLoads() {
