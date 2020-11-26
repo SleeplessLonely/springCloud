3648f24eba5644e1570a802af1b5081bc5d9abec
anxindong <anxdbj@163.com>
2020-11-26 11:23:48 +0800

    turbine测试通过

--- turbine/src/main/java/com/example/demo/TurbineApplication.java
+++ turbine/src/main/java/com/example/demo/TurbineApplication.java
@@ -3,9 +3,11 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.turbine.EnableTurbine;
 @EnableTurbine
 @EnableDiscoveryClient
+@EnableHystrixDashboard
 @SpringBootApplication
 public class TurbineApplication {
 
